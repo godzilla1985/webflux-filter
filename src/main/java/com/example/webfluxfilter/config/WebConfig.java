1cb72db05f53a3e637ae29859d272b42328d121d
EliG <shatikc@gmail.com>
2021-10-18 14:20:20 +0300

    Change Enrich annotation type from String to Enum values

--- src/main/java/com/example/webfluxfilter/config/WebConfig.java
+++ src/main/java/com/example/webfluxfilter/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.example.webfluxfilter.config;
 
 import com.example.webfluxfilter.annotation.EnrichAnnotationMethodArgumentResolver;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 
 @Configuration
+@ConditionalOnProperty
 public class WebConfig implements WebFluxConfigurer {
 
     @Override
